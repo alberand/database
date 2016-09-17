53e8162a03de72d903a77fab06057c5511101f9e
alberand <snashe1@gmail.com>
2016-09-17 18:57:18 +0200

    Add file with airspaces. And small changes.

--- tests/remote_host.py
+++ tests/remote_host.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import time
 import socket
+import pprint
 import logging
 import threading
 
@@ -11,11 +13,20 @@ from packages import sessions_list
 
 logger = logging.getLogger(__name__)
 
+def load_session_from_file(filename):
+    result = list()
+    with open(filename, 'r') as _file:
+        for line in _file.readlines():
+            result.append(line.rstrip('\n'))
+
+    return result
+
 if __name__ == '__main__':
     # Configure test properties
     ip, port = '147.32.196.177', 5000
     # Order: normal, bad, miss, multiple
-    _set = sessions_list[0]
+    # _set = sessions_list[0]
+    _set = load_session_from_file('data.txt')
     # Clients number should be > 0
     clients_number = 1
 
@@ -25,7 +36,7 @@ if __name__ == '__main__':
         client = Device(ip, port, _set)
 
         client.start()
-        clients_list.apeend(client)
+        clients_list.append(client)
 
     # Waiting for all clients to finish
     for client in clients_list:
