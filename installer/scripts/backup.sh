f83d8477ffa31c425585e700c22ca0620db43aaa
alberand <snashe1@gmail.com>
2016-10-17 18:55:24 +0200

    Remove some files.

--- installer/scripts/backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Enter ROOT password of the database (not user)."
-mysqldump --single-transaction --flush-logs --master-data=2 --all-databases -u root -p > ../backups/all_databases.sql
