833053e24bfb01ad7ff511cfd9058148f10dc412
alberand <snashe1@gmail.com>
2016-11-15 18:44:51 +0100

    Random.

--- installer/run_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Temporary script for running server
-
-cd /home/database/database/src/
-python3 ./main.py
--- installer/src/main.py
+++ installer/src/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import config
 import logging.config
 
 from listener import Listener
@@ -8,6 +10,9 @@ from listener import Listener
 logging.config.fileConfig('logging.ini')
 
 if __name__ == '__main__':
+
+    if len(sys.argv) > 1:
+        logging.info('Config file is specified.')
 
     listener = Listener()
     try:
