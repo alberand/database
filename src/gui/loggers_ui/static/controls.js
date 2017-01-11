2236d8e2a002523a013066cf016579287b5bb2c8
alberand <snashe1@gmail.com>
2017-01-11 19:15:54 +0100

    gui: Set aerospace checkbox unchecked by default.

--- src/gui/loggers_ui/static/controls.js
+++ src/gui/loggers_ui/static/controls.js
@@ -1,7 +1,9 @@
 /*
  * Check button which turn on/off aerospaces on the map
  */
-document.getElementById('aerospace_on_btn').onclick = function() {
+// Set checkbox unchecked by default
+var aerospace_cb = document.getElementById('aerospace_on_btn');
+aerospace_cb.onclick = function() {
   if(document.getElementById('aerospace_on_btn').checked) {
     // map.addLayer(aerospace_layer);
     aerospace_layer.setVisible(true);
