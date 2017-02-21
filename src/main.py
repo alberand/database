94c1f2a6028ec1eff64059ee5ec41e0758435019
alberand <snashe1@gmail.com>
2017-02-21 21:36:50 +0100

    src: Help message changes.

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ logging.config.fileConfig('logging.ini')
 if __name__ == '__main__':
     if '-h' in sys.argv or '--help' in sys.argv:
         print('Run this file and as the first argument send name of the config'
-                'file. For example: python main.py config.json')
+              'file. For example: python main.py config.ini')
 
     if len(sys.argv) < 2:
         logging.error('Please specify config name.')
