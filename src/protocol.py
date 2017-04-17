5dfcc2c4405bdb42af33738363f3b420c8a9b4f6
Andrey Albershteyn <alberand@fel.cvut.cz>
2017-04-17 16:22:14 +0200

    Create basic structure and configuration file.

---
+++ src/protocol.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Protocol(object):
+
+    def __init__(self):
+        pass
+
+    def set_definition(self, json_config):
+        """
+        Args:
+            json_config: json object with protocol definition
+        """
+
+        pass
+
+    def put(self):
+        pass
+
+    def get(self):
+        pass
